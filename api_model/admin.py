4e066cce54e041cc076e3955fd8c1003a9413075
FiorellaBonilla <fiorella.bonilla.gonzalez@est.una.ac.cr>
2023-10-31 18:03:18 -0600

    configurate models in site admin

--- api_model/admin.py
+++ api_model/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from api_model.models import Person, Room, Pet, Descriptor
+
 # Register your models here.
+admin.site.register(Person)
+admin.site.register(Room)
+admin.site.register(Pet)
+admin.site.register(Descriptor)
